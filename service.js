ba61486e3e0a38c04d7c498ac2703884a6939550
MikeB <burton.mj@gmail.com>
2017-02-16 18:38:57 -0700

    done

--- service.js
+++ service.js
@@ -1,10 +1,23 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    var deferred = $q.defer();
 
+//move the return to deferred.promise on line 13
+
+    $http({
+        method: 'GET',
+        url: 'https://reqres.in/api/users?page=1'
+    }).then(function(response){
+      var users = response.data.data;
+
+      users.forEach(function(user){
+        user.first_name = "Michael";
+        user.avatar = "http://fatcatart.com/wp-content/gallery/dutch-art/thumbs/thumbs_Helst-Portret-van-Gerard-Andriesz-Bicker-cat-w.jpg";
+      });
+      deferred.resolve(users)
+//above is where you're doing the data manipulation you were doing in your controller on version 2
+    })
+  return deferred.promise;
+  }
 });
